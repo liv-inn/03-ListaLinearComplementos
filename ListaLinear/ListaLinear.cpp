27567a536bc54364096ecc3e6be15563d4410a57
liv-inn <liviaavelino2002@gmail.com>
2024-03-19 11:39:59 -0300

    first commit

--- ListaLinear/ListaLinear.cpp
+++ ListaLinear/ListaLinear.cpp
@@ -122,9 +122,24 @@ void inserirElemento()
 
 void excluirElemento()
 {
+		int valor;
+		cout << "Digite o valor que deseja excluir: ";
+		cin >> valor;
+		int pos = posicaoElemento(valor);
+
+		if (pos != -1) {
+			for (int i = pos; i < nElementos - 1; ++i) {
+				lista[i] = lista[i + 1];
+			}
+			nElementos--;
+			cout << "Elemento removido." << endl;
+		}
+		else {
+			cout << "Elemento nao encontrado." << endl;
+		}
+	}
 
 
-}
 
 void buscarElemento()
 {
